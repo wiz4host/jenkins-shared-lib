6d79d9a96fa37f9aaf77e7ee31fd3950a1f91cda
wiz4host <31190285+wiz4host@users.noreply.github.com>
2021-05-25 22:52:22 +0530

    create welcome.groovy

---
+++ welcome.groovy
@@ -0,0 +1,15 @@
+def call (person) {
+    println "Say hi to " + person
+}
+
+
+def add(int a, int b) {
+     c = a + b
+     //println "sum of " + a + " and " + b + " = " + c
+     return c
+}
+
+
+//welcome ("Adam")
+//sumval = add( 4, 5)
+//println sumval
