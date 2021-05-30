a9480cbb2798f9f4f1ed17d66cb731695b1ac563
wiz4host <31190285+wiz4host@users.noreply.github.com>
2021-05-30 16:06:23 +0530

    Update curlLib.groovy

--- vars/curlLib.groovy
+++ vars/curlLib.groovy
@@ -2,3 +2,10 @@
 def call(){
   println "This is for various curl (webserices) calls"
 }
+
+
+
+def getid (){
+  ID_PROJECT = sh (script: """ curl -k -s "https://official-joke-api.appspot.com/random_joke" | jq '.id' """, returnStdout: true).trim()
+  retrurn ID_PROJECT
+}
