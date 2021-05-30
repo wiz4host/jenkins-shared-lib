32f73298d493a15f35a666528b58a418533d4e85
wiz4host <31190285+wiz4host@users.noreply.github.com>
2021-05-30 18:30:26 +0530

    Update curlLib.groovy

--- vars/curlLib.groovy
+++ vars/curlLib.groovy
@@ -18,6 +18,6 @@ def getidfromenv (){
   script{
         env.JOKE = "random_joke"
     }
-  ID = sh (script: """ curl -k -s "https://official-joke-api.appspot.com/${env.JOKE}" | jq '.id' """, returnStdout: true).trim()
-  return ID
+  IDENV = sh (script: """ curl -k -s "https://official-joke-api.appspot.com/${env.JOKE}" | jq '.id' """, returnStdout: true).trim()
+  return IDENV
 }
